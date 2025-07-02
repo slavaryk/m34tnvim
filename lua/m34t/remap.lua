20f5c6ef18dd81bcff2f9b5669160442e0e3593a
Slava Rykov <v.rykov@lamoda.ru>
2025-07-02 16:27:23 +0500

    Add jump to definition keymap

--- lua/m34t/remap.lua
+++ lua/m34t/remap.lua
@@ -7,4 +7,8 @@ vim.keymap.set("n", "<leader>i", function()
 	vim.diagnostic.open_float(nil, { focus = false, scope = "cursor" })
 end, { desc = "Toggle diagnostics" })
 
+vim.keymap.set("n", "<leader>d", function()
+  vim.lsp.buf.definition()
+end, { desc = "Jump to definition of symbol under cursor" })
+
 vim.keymap.set("t", "<ESC>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
