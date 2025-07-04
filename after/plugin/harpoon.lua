870f2588fbf9cfd67083d9b8a09775055a035ed3
Slava Rykov <v.rykov@lamoda.ru>
2025-07-04 13:18:55 +0500

    Improve harpoon, add removing item keymap

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -5,12 +5,23 @@ local conf = require("telescope.config").values
 harpoon:setup()
 -- REQUIRED
 
--- basic telescope configuration
 local function toggle_telescope(harpoon_files)
   local file_paths = {}
 
   for _, item in ipairs(harpoon_files.items) do
     table.insert(file_paths, item.value)
+  end
+
+  local make_finder = function()
+    local paths = {}
+
+    for _, item in ipairs(harpoon_files.items) do
+      table.insert(paths, item.value)
+    end
+
+    return require("telescope.finders").new_table({
+      results = paths,
+    })
   end
 
   require("telescope.pickers").new({}, {
@@ -20,7 +31,24 @@ local function toggle_telescope(harpoon_files)
     }),
     previewer = conf.file_previewer({}),
     sorter = conf.generic_sorter({}),
-  }):find()
+    layout_strategy = "center",
+    initial_mode = "normal",
+    previewer = false,
+    attach_mappings = function(prompt_buffer_number, map)
+      -- Remove entry
+      map("n", "<C-d>", function()
+        local state = require("telescope.actions.state")
+        local selected_entry = state.get_selected_entry()
+        local current_picker = state.get_current_picker(prompt_buffer_number)
+
+        harpoon:list():remove(selected_entry)
+        current_picker:refresh(make_finder())
+      end
+    )
+
+    return true
+  end,
+}):find()
 end
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
