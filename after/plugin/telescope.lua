aeaba6a45699e5210552ba06535044b5f467eb73
Slava Rykov <v.rykov@lamoda.ru>
2025-07-24 11:41:12 +0500

    Add telescope grep

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -6,3 +6,5 @@ vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Telescope git files" }
 vim.keymap.set("n", "<leader>pg", function()
   builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end, { desc = "Telescope live grep but with vim grep" })
+
+vim.keymap.set("n", "<leader>ps", builtin.grep_string, { desc = "Telescope grep" })
