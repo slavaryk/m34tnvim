2a6362d716c298673996f6274bbb3ae7ac52148a
Slava Rykov <v.rykov@lamoda.ru>
2025-08-26 16:19:55 +0500

    Fix telescope empty search

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,7 +4,12 @@ vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Telescope find f
 vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Telescope git files" })
 
 vim.keymap.set("n", "<leader>pg", function()
-  builtin.grep_string({ search = vim.fn.input("Grep > ") })
+  local search_string = vim.fn.input("Grep > ")
+
+  if (search_string ~= "" and search_string ~= nil) then
+    print("wtf")
+    builtin.grep_string({ search = search_string })
+  end
 end, { desc = "Telescope live grep but with vim grep" })
 
 vim.keymap.set("n", "<leader>ps", builtin.grep_string, { desc = "Telescope grep" })
