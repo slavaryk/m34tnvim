7478136fa18a82a4df89cc8c96784d92ca3cba37
slavar <m3atyy@gmail.com>
2025-08-27 20:37:43 +0500

    Fix completion and remove unused keymaps

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -57,8 +57,3 @@ end
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end)
-
-vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
